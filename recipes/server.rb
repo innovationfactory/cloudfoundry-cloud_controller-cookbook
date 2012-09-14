0bd659a93aae0506dfb4bdd84ce269d32cb86a58
Ruben Koster <ruben.koster@innovationfactory.eu>
2012-09-14 10:23:33 +0200

    Changed cloud_controller paths a refelect repo structure

--- recipes/server.rb
+++ recipes/server.rb
@@ -18,16 +18,21 @@ package "libxslt1-dev"
 package "sqlite3"
 package "libsqlite3-dev"
 
+ruby_version = node.cloudfoundry_common.ruby_1_9_2_version
+install_path = File.join(node[:cloudfoundry_common][:vcap][:install_path], "cloud_controller", "cloud_controller")
+
 cloudfoundry_component "cloud_controller" do
   pid_file node.cloudfoundry_cloud_controller.server.pid_file
   log_file node.cloudfoundry_cloud_controller.server.log_file
+  binary "RBENV=#{ruby_version} ruby #{File.join(install_path, "bin", "cloud_controller")}"
+  install_path install_path
 end
 
 bash "run cloudfoundry migrations" do
   user node[:cloudfoundry_common][:user]
-  cwd  File.join(node[:cloudfoundry_common][:vcap][:install_path], "cloud_controller")
+  cwd  File.join(node[:cloudfoundry_common][:vcap][:install_path], "cloud_controller", "cloud_controller")
   code "RBENV_VERSION='#{ruby_version}' bundle exec rake db:migrate RAILS_ENV=production CLOUD_CONTROLLER_CONFIG='#{config_file}'"
-  subscribes :run, resources(:git => node[:cloudfoundry_common][:vcap][:install_path])
+  subscribes :run, resources(:git => "#{File.join(node[:cloudfoundry_common][:vcap][:install_path], "cloud_controller")}")
   action :nothing
 end
 
