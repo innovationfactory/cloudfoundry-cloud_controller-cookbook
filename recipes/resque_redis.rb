232e4147568a47f804f0765f578ab011742da118
Ruben Koster <ruben.koster@innovationfactory.eu>
2012-09-25 11:02:16 +0200

    Added upstart redis conf and added redis to cc config

--- recipes/resque_redis.rb
+++ recipes/resque_redis.rb
@@ -16,33 +16,58 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-resque_redis_settings = {
-  'user' => node[:cloudfoundry_common][:user],
-  'configdir' => '/etc/redis',
-  'address' => node[:ipaddress] ,
-  'backuptype' => 'rdb', 
-  'save' => ['900 1','300 10','60 10000'], 
-  'datadir' => node['cloudfoundry_cloud_controller']['resque_redis']['datadir'],
-  'timeout' => '600', 
-  'loglevel' => 'notice', 
-  'logfile' => '/var/log/redis', 
-  'requirepass' => node[:cloudfoundry_cloud_controller][:resque_redis[:password]
-}
+version = node[:cloudfoundry_cloud_controller][:resque_redis][:version]
 
-server = [{ 
-   'address' => node[:ipaddress],
-   'port' => '6379' 
-}]
-
-version = node['cloudfoundry_cloud_controller']['resque_redis']['version']
-
-redisio_install "redis-servers" do 
+redisio_install 'redis-servers' do 
   version version
-  download_url 'http://redis.googlecode.com/files/redis-#{version}.tar.gz' 
-  default_settings resque_redis_settings
-  servers server
+  download_url "http://redis.googlecode.com/files/redis-#{version}.tar.gz"
+  default_settings {}
+  servers []
   safe_install false 
   base_piddir '/var/run'
 end
 
+config_dir = node[:cloudfoundry_cloud_controller][:resque_redis][:config_dir]
 
+data_dir = node[:cloudfoundry_cloud_controller][:resque_redis][:data_dir]
+
+[config_dir, data_dir].each do |dir|
+  directory dir do
+    owner node[:cloudfoundry_common][:user]
+    mode '0775'
+    recursive true
+    action :create
+  end
+end
+
+config_file = File.join(config_dir, 'resque_redis.conf')
+
+template config_file do
+  source 'redis.conf.erb'
+  owner node[:cloudfoundry_common][:user]
+  mode '0644'
+  variables({
+    :pid_file     => node[:cloudfoundry_cloud_controller][:resque_redis][:pid_file],
+    :port         => node[:cloudfoundry_cloud_controller][:resque_redis][:port],
+    :address      => node[:ipaddress],
+    :requirepass  => node[:cloudfoundry_cloud_controller][:resque_redis][:password],
+    :data_dir      => node[:cloudfoundry_cloud_controller][:resque_redis][:data_dir],
+  })
+  notifies :restart, 'service[resque-redis]'
+end
+
+template '/etc/init/resque-redis.conf' do
+  source 'redis.init.conf.erb'
+  owner node[:cloudfoundry_common][:user]
+  mode '0644'
+  variables({
+    :config_file => config_file
+  })
+  notifies :restart, 'service[resque-redis]'
+end
+
+service 'resque-redis' do
+  provider Chef::Provider::Service::Upstart
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :restart ]
+end
